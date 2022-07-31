7e24c39c75ad749c6b788d0e2adf9e24c59a1bab
lumcab <lumcab@gmail.com>
2022-07-31 17:57:57 -0500

    cambio

---
+++ Python/Tarea1.py
@@ -0,0 +1,13 @@
+variable1 = "Cadena de texto"
+print(variable1)
+print(type (variable1))
+print(id (variable1))
+variable1 = "Cadena de texto modificada"
+print(variable1)
+print(type (variable1))
+print(id (variable1))
+diccionario = {'valor1':1, 'valor2':2, 'valor3':3, 'valor4':4}
+print(diccionario)
+valorborrado = diccionario.pop('valor3')
+print(diccionario)
+print(valorborrado)
